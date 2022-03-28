08fb8b0a8dd52a8611ba067311fe44944028acdf
Marat <helvegen82@gmail.com>
2022-03-28 18:17:25 +0300

    added login and logout views. main blueprit,index.html and index view

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,27 @@
-from flask import render_template
+from flask import render_template, url_for, flash, redirect, request
+from flask_login import login_user, login_required
 from . import auth
+from ..models import User
+from .forms import LoginForm
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('base.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data.lower()).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            next = request.args.get('next')
+            if next is None or next.startswith('/'):
+                next = url_for('main.index')
+            return redirect(next)
+        flash('Invalid email or password')
+    return render_template('auth/login.html', form=form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    login_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.index'))
