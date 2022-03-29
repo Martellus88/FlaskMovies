325f8fc0580fa7a27b49623ded8ead75e2df5817
Marat <helvegen82@gmail.com>
2022-03-29 20:32:55 +0300

    added user profile

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,7 @@ db = SQLAlchemy()
 
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
+login_manager.session_protection = 'strong'
 
 
 def create_app(config_name):
--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,14 @@
 from flask import render_template
 from . import main
+from ..models import User
+
 
 @main.route('/')
 def index():
     return render_template('index.html')
+
+
+@main.route('/profile/<username>')
+def profile_username(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('profile.html', user=user)
