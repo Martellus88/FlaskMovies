995d9482fbcc68ca317cf63acd6bd722fb435254
Marat <helvegen82@gmail.com>
2022-03-30 19:45:09 +0300

    added edit profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,9 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from flask_login import current_user
 from . import main
 from ..models import User
+from .forms import EditProfileForm
+from .. import db
 
 
 @main.route('/')
@@ -12,3 +15,20 @@ def index():
 def profile_username(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('profile.html', user=user)
+
+
+@main.route('/edit_profile', methods=['GET', 'POST'])
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        db.session.commit()
+        flash('Your profile has been updated.')
+        return redirect(url_for('.profile_username', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
