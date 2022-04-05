3797c7515453222d3fb9b805ab13cfb7594a3ab1
Marat <helvegen82@gmail.com>
2022-04-05 21:44:12 +0300

    create init_app and SMTP logging

--- app/__init__.py
+++ app/__init__.py
@@ -17,6 +17,7 @@ login_manager.session_protection = 'strong'
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
 
     bootstrap.init_app(app)
     mail.init_app(app)
--- config.py
+++ config.py
@@ -11,11 +11,15 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASK_MOVIE_SUBJECT = ['FlaskMovies']
+    FLASK_MOVIE_SUBJECT = '[FlaskMovies]'
     FLASK_MOVIE_ADMIN = os.environ.get('FLASK_MOVIE_ADMIN')
     FLASK_MOVIE_SENDER = 'Flask Movies'
 
     MOVIES_PER_PAGE = 15
+
+    @staticmethod
+    def init_app(app):
+        pass
 
 
 class DevelopmentConfig(Config):
@@ -33,6 +37,29 @@ class TestingConfig(Config):
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+
+        import logging
+        from logging.handlers import SMTPHandler
+        credentials = None
+        secure = None
+        if getattr(cls, 'MAIL_USERNAME', None) is not None:
+            credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+            if getattr(cls, 'MAIL_USE_TLS', None):
+                secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+            fromaddr=cls.FLASK_MOVIE_SENDER,
+            toaddrs=[cls.FLASK_MOVIE_ADMIN],
+            subject=cls.FLASK_MOVIE_SUBJECT + ' Application Error',
+            credentials=credentials,
+            secure=secure
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
 
 
 config = {
